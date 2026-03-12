df36df232c13d2f9aeee1b48091db48f351ca5b7
anunymous <anurg@yahoo.com>
2026-03-12 01:17:30 +0000

    ..

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
-
-load_dotenv()
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+load_dotenv()
 
 # Initialize the ChatAnthropic model.
 # The API key will be automatically picked up from the ANTHROPIC_API_KEY environment variable.
