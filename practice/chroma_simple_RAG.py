29c90e39717c350d277d10f0d447438980d35529
anunymous <anurg@yahoo.com>
2026-05-15 17:47:15 +0000

    ...

--- practice/chroma_simple_RAG.py
+++ practice/chroma_simple_RAG.py
@@ -17,9 +17,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from dotenv import load_dotenv
-
-load_dotenv()
-
+import os
+load_dotenv(override=True)
+print(os.environ.get("OPENAI_API_KEY"))
 # ─────────────────────────────────────────────────────────────────
 # Config
 # ─────────────────────────────────────────────────────────────────
